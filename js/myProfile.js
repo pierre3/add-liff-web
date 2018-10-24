ab36895d03affb2d74067810beb24ca5b50115a0
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 17:57:23 +0900

    test

--- js/myProfile.js
+++ js/myProfile.js
@@ -9,14 +9,15 @@ window.onload = function (e) {
     
 };
 
-function initializeApp(data, profile) {
+function initializeApp(data) {
     //var profile = liff.getProfile();
     //document.getElementById('userimagefield').src = profile.pictureUrl;
     //document.getElementById('usernamefield').textContent = profile.displayName;
    // document.getElementById('messagefield').textContent = profile.statusMessage;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+    alert(JSON.parse(data));
+    $('#useridfield').val(data.context.userId);
+    $('#utouidfield').val(data.context.utouId);
+    $('#roomidfield').val(data.context.roomId);
+    $('#groupidfield').val(data.context.groupId);
 }
 
