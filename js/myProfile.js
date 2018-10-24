2a8fcbe8204aa680728c7e12cf27290cc3bc6efe
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 17:44:42 +0900

    update page layout

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,14 +1,20 @@
 window.onload = function (e) {
 
-    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
     liff.init(function (data) {
         initializeApp(data);
     });
+
+    
+
+    
 };
 
-function initializeApp(data) {
+function initializeApp(data, profile) {
+    var profile = liff.getProfile();
+    document.getElementById('userimagefield').src = profile.pictureUrl;
+    document.getElementById('usernamefield').textContent = profile.displayName;
+    document.getElementById('messagefield').textContent = profile.statusMessage;
     document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
     document.getElementById('useridfield').textContent = data.context.userId;
     document.getElementById('utouidfield').textContent = data.context.utouId;
     document.getElementById('roomidfield').textContent = data.context.roomId;
