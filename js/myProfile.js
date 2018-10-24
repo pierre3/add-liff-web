7c8f75998e42700b892b830dee8c0c5fa94028f6
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 18:58:21 +0900

    test

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,13 +1,11 @@
 window.onload = function () {
-
-    liff.init(function (data) {
-        initializeApp(data);
-    });
-};
-
-function initializeApp(data) {
     $('#useridfield').val('user id');
     $('#utouidfield').val('utou id');
     $('#roomidfield').val('room id');
     $('#groupidfield').val('group id');
+    
+};
+
+function initializeApp(data) {
+    
 }
