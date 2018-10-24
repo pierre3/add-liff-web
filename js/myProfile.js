163b9d0d5793926c5846eb4fdd009a22cbc2b8f5
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 18:32:54 +0900

    test

--- js/myProfile.js
+++ js/myProfile.js
@@ -10,4 +10,12 @@ function initializeApp(data) {
     $('#utouidfield').val(data.context.utouId);
     $('#roomidfield').val(data.context.roomId);
     $('#groupidfield').val(data.context.groupId);
+    
+    liff.getProfile().then(function (profile) {
+        $('#userimagefield').attr('src', profile.pictureUrl);
+        $('#usernamefield').val(profile.displayName);
+        $('#messagefield').val(profile.statusMessage);
+    }).catch(function (error) {
+        window.alert("Error getting profile: " + error.message);
+    });
 }
