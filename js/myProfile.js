e2017a795a71bb95ce4667a7a9a92f729b637ace
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 18:07:59 +0900

    test

--- js/myProfile.js
+++ js/myProfile.js
@@ -10,11 +10,12 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    var profile = liff.getProfile();
-    alert(JSON.parse(profile));
-    $('#userimagefield').attr('src', profile.pictureUrl);
-    $('#usernamefield').val(profile.displayName);
-    $('#messagefield').val(profile.statusMessage);
+    liff.getProfile().then(function(profile)
+    {
+        $('#userimagefield').attr('src', profile.pictureUrl);
+        $('#usernamefield').val(profile.displayName);
+        $('#messagefield').val(profile.statusMessage);
+    });
     $('#useridfield').val(data.context.userId);
     $('#utouidfield').val(data.context.utouId);
     $('#roomidfield').val(data.context.roomId);
