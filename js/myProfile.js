14f1f211ac1dfb9c5c48fb1f3265aabd9c17a2a0
寛忠 小林 <kobayashi@implem.co.jp>
2018-10-24 17:49:23 +0900

    test

--- js/myProfile.js
+++ js/myProfile.js
@@ -11,9 +11,9 @@ window.onload = function (e) {
 
 function initializeApp(data, profile) {
     var profile = liff.getProfile();
-    document.getElementById('userimagefield').src = profile.pictureUrl;
-    document.getElementById('usernamefield').textContent = profile.displayName;
-    document.getElementById('messagefield').textContent = profile.statusMessage;
+    //document.getElementById('userimagefield').src = profile.pictureUrl;
+    //document.getElementById('usernamefield').textContent = profile.displayName;
+   // document.getElementById('messagefield').textContent = profile.statusMessage;
     document.getElementById('languagefield').textContent = data.language;
     document.getElementById('useridfield').textContent = data.context.userId;
     document.getElementById('utouidfield').textContent = data.context.utouId;
